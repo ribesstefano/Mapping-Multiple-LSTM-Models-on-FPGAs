513c095aaf6c0010e53f875d8fb02dfde8ddf50d
stefanoribes <ribes.stefano@gmail.com>
2021-08-27 11:35:46 +0200

    KernelV: correct simulation for single input.

--- src/testbenches/test_v_kernel.cpp
+++ src/testbenches/test_v_kernel.cpp
@@ -13,31 +13,43 @@ int main(int argc, char const *argv[]) {
 #ifdef COSIM_DESIGN
   srand(1);
 #else
-  srand(time(NULL));
+  srand(1);
+  // srand(time(NULL));
 #endif
   std::cout << "[INFO] Starting HlsKernelV test." << std::endl;
 #ifndef __VITIS_HLS__
   return 0;
 #else
-  const int num_refinements_max = testv::params::R;
-  hls::vector<int, testv::params::N> num_refinements_vect = hls::vector<int, testv::params::N>(num_refinements_max);
+  int num_active_inputs = testv::params::N;
+  int output_size = testv::params::H;
+  int num_refinements = testv::params::R;
+  if (argc >= 2) {
+    num_active_inputs = atoi(argv[1]);
+  }
+  if (argc >= 3) {
+    output_size = atoi(argv[2]);
+  }
+  if (argc >= 4) {
+    num_refinements = atoi(argv[3]);
+  }
+  const int kNumRefinementsMax = num_refinements;
+  hls::vector<int, testv::params::N> num_refinements_vect = hls::vector<int, testv::params::N>(kNumRefinementsMax);
   // for (int i = testv::params::N; i >= 0; --i) {
-  //   int R_tmp = num_refinements_max - 2 * (testv::params::N - i - 1);
+  //   int R_tmp = kNumRefinementsMax - 2 * (testv::params::N - i - 1);
   //   num_refinements_vect[i] = R_tmp > 0 ? R_tmp : 1;
   // }
   const int kNumTests = 2;
-  const int kNumActiveInputs = 1; // testv::params::N - 0;
-  const int kOutputSize_tmp = testv::params::H / 1;
-  const int kOutputSize = (kOutputSize_tmp > testv::params::H) ? testv::params::H : kOutputSize_tmp;
+  const int kNumActiveInputs = (num_active_inputs > testv::params::N) ? testv::params::N : num_active_inputs;
+  const int kOutputSize = (output_size > testv::params::H) ? testv::params::H : output_size;
   const int kNumTilesV = kOutputSize / testv::params::Tv;
   typedef typename testv::params::ActivationD ActivationType;
   assert(testv::params::H == testv::params::PrunedSizeV); // No pruning.
 
-  ActivationType xus[num_refinements_max][testv::params::N][testv::params::G] = {ActivationType(0.001)};
-  ActivationType v[num_refinements_max][testv::params::PrunedSizeV][testv::params::G] = {ActivationType(0.001)};
+  ActivationType xus[kNumRefinementsMax][testv::params::N][testv::params::G] = {ActivationType(0.001)};
+  ActivationType v[kNumRefinementsMax][testv::params::PrunedSizeV][testv::params::G] = {ActivationType(0.001)};
   ActivationType y_gold[testv::params::N][testv::params::G][testv::params::H] = {0};
 
-  for (int i = 0; i < num_refinements_max; ++i) {
+  for (int i = 0; i < kNumRefinementsMax; ++i) {
     for (int j = 0; j < testv::params::G; ++j) {
       for (int k = 0; k < testv::params::N; ++k) {
         xus[i][k][j] = rand() * 0.00001;
@@ -48,7 +60,7 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  for (int i = 0; i < num_refinements_max; ++i) {
+  for (int i = 0; i < kNumRefinementsMax; ++i) {
     for (int j = 0; j < kNumActiveInputs; ++j) {
       if (i < num_refinements_vect[j]) {
         for (int k = 0; k < kOutputSize; ++k) {
@@ -72,7 +84,7 @@ int main(int argc, char const *argv[]) {
   for (int t = 0; t < kNumTests; ++t) {
     // Stream XUS
     typename testv::params::VectG_Type xus_val;
-    for (int i = 0; i < num_refinements_max; ++i) {
+    for (int i = 0; i < kNumRefinementsMax; ++i) {
       for (int j = 0; j < kNumActiveInputs; ++j) {
         if (i < num_refinements_vect[j]) {
           for (int k = 0; k < testv::params::G; ++k) {
@@ -84,18 +96,22 @@ int main(int argc, char const *argv[]) {
     }
     // Stream V
     typename testv::params::VectTvType v_val;
-    for (int i = 0; i < num_refinements_max; ++i) {
+    for (int i = 0; i < kNumRefinementsMax; ++i) {
       for (int k = 0; k < kNumTilesV; ++k) {
         for (int j = 0; j < testv::params::G; ++j) {
           for (int ii = 0; ii < testv::params::Tv; ++ii) {
-            v_val[ii] = v[i][k][k * testv::params::Tv + ii];
+            v_val[ii] = v[i][k * testv::params::Tv + ii][j];
           }
           v_axis.PushVector<ActivationType, testv::params::Tv>(v_val);
         }
       }
     }
+  }
+  for (int t = 0; t < kNumTests; ++t) {
     std::cout << "[INFO] Starting HlsKernelV." << std::endl;
     HlsKernelV(kNumActiveInputs, kOutputSize, num_refinements_vect, xus_port, v_port, y_port);
+  }
+  for (int t = 0; t < kNumTests; ++t) {
     std::cout << "[INFO] Checking results test n." << t << std::endl;
     int test_errors = 0;
     for (int j = 0; j < kNumTilesV; ++j) {
@@ -104,11 +120,10 @@ int main(int argc, char const *argv[]) {
         auto y_val = y_axis.PopVector<ActivationType, kGTv>();
         for (int k = 0; k < testv::params::Tv; ++k) {
           for (int ii = 0; ii < testv::params::G; ++ii) {
-            std::cout << i << ") test/gold: "
-                      << y_val[k * testv::params::G + ii] << " / "
-                      << y_gold[i][ii][j * testv::params::Tv + k] << std::endl;
-            // ActivationType y_gold[testv::params::N][testv::params::G][testv::params::H] = {0};
             if (y_val[k * testv::params::G + ii] != y_gold[i][ii][j * testv::params::Tv + k]) {
+              std::cout << i << ") test/gold: "
+                        << y_val[k * testv::params::G + ii] << " / "
+                        << y_gold[i][ii][j * testv::params::Tv + k] << std::endl;
               ++test_errors;
             }
           }
