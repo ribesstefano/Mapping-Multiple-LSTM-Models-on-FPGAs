fce3883632922375dbd93a3d2973bda0dc2a2d0d
stefanoribes <ribes.stefano@gmail.com>
2021-09-11 18:56:40 +0200

    HlsSvdKernel: added testbench.

---
+++ include/testbenches/test_svd_kernel.h
@@ -0,0 +1,7 @@
+#ifndef TESTBENCHES_TEST_SVD_KERNEL_H_
+#define TESTBENCHES_TEST_SVD_KERNEL_H_
+
+#include "svd_params.h"
+#include "kernel/svd_kernel.h"
+
+#endif // end TESTBENCHES_TEST_SVD_KERNEL_H_
